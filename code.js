485f8446f51ab7f05b108894efa8e014174e3c64
Peanut7567 <markjh1808@gmail.com>
2024-03-13 22:43:53 +0000

    Changes

---
+++ code.js
@@ -0,0 +1,24 @@
+function signUp() {
+
+    document.getElementById("test").innerHTML = "Google";
+
+}
+
+
+function logIn() {
+
+    document.getElementById("test").innerHTML = "A";
+
+}
+
+
+function passwordChecker() {
+
+
+}
+
+
+function passwordManager() {
+
+
+}
