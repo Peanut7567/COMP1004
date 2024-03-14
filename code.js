41a588c0e51bf933546d7f2a26e06d430f59f314
Peanut7567 <markjh1808@gmail.com>
2024-03-14 14:22:23 +0000

    Update code.js

--- code.js
+++ code.js
@@ -22,3 +22,14 @@ function passwordManager() {
 
 
 }
+
+
+// Exports data stored in json_data.users for the user to download
+var json_object = JSON.stringify(json_data.users); //Store usernames and passwords which enter into table
+const blob = new Blob([json_object], {type: 'application/json'});
+var anchor = document.createElement("a");
+anchor.download = "data.json";
+anchor.href = window.URL.createObjectURL(blob);
+anchor.innerHTML = "download";
+anchor.click();
+console,log(anchor);
